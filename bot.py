e24e8ac95648be7dfbbd744730c2dc42ba0f047b
Alexander Greco <33609792+Alexander5015@users.noreply.github.com>
2020-01-23 11:07:35 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,8 +3,11 @@ import random
 import asyncio
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix='$', description='A bot that posts raffles.')
+from datetime import datetime
+import pytz
 
+bot = commands.Bot(command_prefix='$', description='A bot that posts raffles.')
+override = False
 
 @bot.event
 async def on_ready():
@@ -13,58 +16,69 @@ async def on_ready():
     print(bot.user.id)
     print('------')
 
-
+@bot.command(pass_context=True)
+@commands.cooldown(1, 30, commands.BucketType.user)
+async def overrideAssassinate(ctx):
+    override = !override
+    await ctx.send("Assassinate Override set to: " + override)
+    
 @bot.command(pass_context=True)
 @commands.cooldown(1, 30, commands.BucketType.user)
 async def assassinate(ctx, user: str):
-    server = bot.get_guild(667585691621916702)
-    announcements = bot.get_channel(668143653709152296)
-    assassinLog = bot.get_channel(668323099288010752)
-    x = server.members
-    assassin = ctx.message.author
-    odds = random.randint(0,100)
-    targetMember = server.get_member_named(user)
+    
+    tz_MST = pytz.timezone('Canada/Mountain') 
+    datetime_Calgary = datetime.now(tz_MST)
+    hour = datetime_Calgary.hour()
+    timeCheck = (hour >= 18 or  hour <9)
+    if timeCheck or override:
+        server = bot.get_guild(667585691621916702)
+        announcements = bot.get_channel(668143653709152296)
+        assassinLog = bot.get_channel(668323099288010752)
+        x = server.members
+        assassin = ctx.message.author
+        odds = random.randint(0,100)
+        targetMember = server.get_member_named(user)
 
-    global y
-    y = []
-    for member in x:
-        role = discord.utils.find(lambda r: r.name == 'Nobility', server.roles)
-        if role in member.roles and member != assassin and member != targetMember:
+        global y
+        y = []
+        for member in x:
+            role = discord.utils.find(lambda r: r.name == 'Nobility', server.roles)
+            if role in member.roles and member != assassin and member != targetMember:
                 y.append(member.mention)            
-    random.shuffle(y)
+        random.shuffle(y)
     
-    y = [y[0],y[1],y[2],assassin.mention]
-    random.shuffle(y)
+        y = [y[0],y[1],y[2],assassin.mention]
+        random.shuffle(y)
 
-    await assassinLog("Assassin: " + assasin.mention + "Target: " + targetMember.mention)
+        await assassinLog("Assassin: " + assasin.mention + "Target: " + targetMember.mention)
     
-    if odds < 15:
-        roles = targetMember.roles
-        everyone = discord.utils.get(server.roles, name ="@everyone")
-        roles.remove(everyone)
-        await targetMember.remove_roles(*roles)
-        dead = discord.utils.get(server.roles, name="Talking Dead")
-        await targetMember.add_roles(dead)
-        await announcements.send(assassin.mention + " assassinated " + targetMember.mention + "!")
+        if odds < 15:
+            roles = targetMember.roles
+            everyone = discord.utils.get(server.roles, name ="@everyone")
+            roles.remove(everyone)
+            await targetMember.remove_roles(*roles)
+            dead = discord.utils.get(server.roles, name="Talking Dead")
+            await targetMember.add_roles(dead)
+            await announcements.send(assassin.mention + " assassinated " + targetMember.mention + "!")
         
-    elif odds < 30:
-        roles = targetMember.roles
-        everyone = discord.utils.get(server.roles, name ="@everyone")
-        roles.remove(everyone)
-        await targetMember.remove_roles(*roles)
-        dead = discord.utils.get(server.roles, name="Talking Dead")
-        await targetMember.add_roles(dead)
+        elif odds < 30:
+            roles = targetMember.roles
+            everyone = discord.utils.get(server.roles, name ="@everyone")
+            roles.remove(everyone)
+            await targetMember.remove_roles(*roles)
+            dead = discord.utils.get(server.roles, name="Talking Dead")
+            await targetMember.add_roles(dead)
 
-        await announcements.send("Someone assassinated " + targetMember.mention + "!")
-        await announcements.send("Suspects are " + y[0] + ", " + y[1] + ", " +
+            await announcements.send("Someone assassinated " + targetMember.mention + "!")
+            await announcements.send("Suspects are " + y[0] + ", " + y[1] + ", " +
                                  y[2] + ", " + y[3])
         
-    elif odds < 65:
-        await announcements.send(assassin.mention + " attempted to assassinate " + targetMember.mention + "!")
+        elif odds < 65:
+            await announcements.send(assassin.mention + " attempted to assassinate " + targetMember.mention + "!")
         
-    else:
-        await announcements.send("Someone attempted to assassinate " + targetMember.mention + "!")
-        await announcements.send("Suspects are " + y[0] + ", " + y[1] + ", " +
+        else:
+            await announcements.send("Someone attempted to assassinate " + targetMember.mention + "!")
+            await announcements.send("Suspects are " + y[0] + ", " + y[1] + ", " +
                                  y[2] + ", " + y[3])
 @bot.event
 async def on_command_error(error, ctx):
